51223d552c8bbae4e3e86e3d331125070857ab0a
aldyazarya <aldy1612@gmail.com>
2020-06-13 20:08:35 +0800

    sending and ordering chat messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../widgets/chat/messages.dart';
+import '../widgets/chat/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -41,16 +42,10 @@ class ChatScreen extends StatelessWidget {
             Expanded(
               child: Messages(),
             ),
+            NewMessage(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            Firestore.instance
-                .collection('chats/fWRMzk8qApsATv4YONfz/messages')
-                .add({'text': 'This was added by clicking the button'});
-          }),
     );
   }
 }
