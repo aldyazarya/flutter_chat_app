6e180bcdaabbff99c729c806584f9958e5f9d822
aldyazarya <aldy1612@gmail.com>
2020-06-13 18:00:01 +0800

    setting up for chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../widgets/chat/messages.dart';
+
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -33,29 +35,14 @@ class ChatScreen extends StatelessWidget {
           },
         ),
       ]),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance
-            .collection('chats/fWRMzk8qApsATv4YONfz/messages')
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.hasError)
-            return new Text('Error: ${streamSnapshot.error}');
-          switch (streamSnapshot.connectionState) {
-            case ConnectionState.waiting:
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            default:
-              final documents = streamSnapshot.data.documents;
-              return ListView.builder(
-                itemCount: documents.length,
-                itemBuilder: (ctx, index) => Container(
-                  padding: EdgeInsets.all(8),
-                  child: Text(documents[index]['text']),
-                ),
-              );
-          }
-        },
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Messages(),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
